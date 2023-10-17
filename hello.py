24c52b225f5e9bb4816bf8d361c4c72661bd19d5
cristianFedora <cristianoso19@gmail.com>
2023-10-16 22:44:46 -0500

    Print variables with comas

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
-name = input ("What's your name?") #Variables
+name = input ("What's your name? ") #Variables
 
-print("hello, " + name )
+print("hello,", name) #Py add automatically a space when pass arguments with commas
