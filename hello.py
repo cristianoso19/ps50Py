882b61140bc7c426ee9bec55e2c061a3f85b7c63
cristianFedora <cristianoso19@gmail.com>
2023-10-16 22:58:02 -0500

    print passing arguments to print function

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 name = input ("What's your name? ") #Variables
 
-print("hello,", name) #Py add automatically a space when pass arguments with commas
+print("hello,", end="") #Py print function recibes as argument separator (sep='') or end (end="\n")
+print(name)
