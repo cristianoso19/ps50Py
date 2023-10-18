72032cec0ef1f7c612e2346a6093350eacb73831
cristianFedora <cristianoso19@gmail.com>
2023-10-17 22:20:26 -0500

    Str split

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 name = input("What's your name? ").strip().title()
 
+# split user's name into first name and last name
+first, last = name.split(" ")
+
 #Remove whitespace from str and capitallize 
 #name = name.strip().title() #method
 
-print(f"hello, {name}") #Py new feature f string
+print(f"hello, {first}") #Py new feature f string
