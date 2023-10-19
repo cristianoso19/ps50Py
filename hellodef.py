1433fd0abef3ced8121cfe45db0090f1db164f7b
cristianFedora <cristianoso19@gmail.com>
2023-10-18 22:20:11 -0500

    Scoope

--- hellodef.py
+++ hellodef.py
@@ -5,6 +5,7 @@ def main():
 
 def hello(to="world"): # to="WORLD" is default value of parameter
     print("hello, ", to) #The identetion is very important!
+#Scope: the variable exists only int the function where it is created
 
 main() #call the main function in the bottom 
  
