12b02baddbda45e8ae9ca87a475bce5ca3e156b2
cristianFedora <cristianoso19@gmail.com>
2023-10-18 22:14:55 -0500

    Functions in python

---
+++ hellodef.py
@@ -0,0 +1,10 @@
+def main ():
+    hello() #Call to function with no arguments
+    name = input("What's your name? ")
+    hello(name)
+
+def hello(to="world"): # to="WORLD" is default value of parameter
+    print("hello, ", to) #The identetion is very important!
+
+main() #call the main function in the bottom 
+ 
