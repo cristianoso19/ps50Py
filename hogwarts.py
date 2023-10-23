deeb75ab2627c6ccbc5db31785161942707dfb63
cristianFedora <cristianoso19@gmail.com>
2023-10-22 22:06:34 -0500

    Loops: Lists of dictonaries and iterate them

--- hogwarts.py
+++ hogwarts.py
@@ -1,9 +1,9 @@
-students = {
-    "Hermione": "Gryffindor", 
-    "Harry": "Gryffidor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin"    
-}
+students = [
+    {"name": "Hermione", "house":"Gryffindor", "patronus":"Otter"},
+    {"name": "Harry", "house":"Gryffindor", "patronus":"Stag"},
+    {"name": "Ron", "house":"Gryffindor", "patronus":"Jack Rusell Terrier"},
+    {"name": "Draco", "house":"Slytherin", "patronus":None}, #None ausence of a value
+]
 
 for student in students:
-    print(student, students[student], sep=", ")
+    print(student["name"], student["house"], student["patronus"], sep=", ")
