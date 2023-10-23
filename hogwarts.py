2b46f392b4c9463818e29c114380766efdb84121
cristianFedora <cristianoso19@gmail.com>
2023-10-22 21:10:57 -0500

    Loops: Lists, use of len and for for iterate list

--- hogwarts.py
+++ hogwarts.py
@@ -1,4 +1,4 @@
 students = ["Hermione", "Harry", "Ron"]
 
-for stundent in students:
-    print(stundent)
+for i in range(len(students)): #len returns the length of a list
+    print(i + 1, students[i])
