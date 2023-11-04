0aa57c808dd2dda080d97494e5ecc5e5d219a51d
cristianFedora <cristianoso19@gmail.com>
2023-11-03 22:48:26 -0500

    While true break

--- exhello.py
+++ exhello.py
@@ -1,18 +1,27 @@
 print("hello, world")
 # Syntax error, error  de sintaxis
 # valueError, error de tipo de dato en variables
-try:
-    x = int(input("What's x? "))
-    print (f"x is {x}")
-except ValueError:
-    print("x is not integer")
+# try:
+#     x = int(input("What's x? "))
+#     print (f"x is {x}")
+# except ValueError:
+#     print("x is not integer")
 
 # NameError: problem with the names of varieables
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("x is not integer")
-else: #Executes if no error was found
-    print (f"x is {x}")
+# try:
+#     x = int(input("What's x? "))
+# except ValueError:
+#     print("x is not integer")
+# else: #Executes if no error was found
+#     print (f"x is {x}")
 
 #
+while True:
+    try:
+        x = int(input("What's x? "))
+    except ValueError:
+        print("x is not integer")
+    else: #Executes if no error was found
+        break
+
+print (f"x is {x}")
