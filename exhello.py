83e9a69dd1e4705ca5a4863e1fc6be589e8c4b19
cristianFedora <cristianoso19@gmail.com>
2023-11-03 23:09:08 -0500

    getInt

--- exhello.py
+++ exhello.py
@@ -16,12 +16,25 @@ print("hello, world")
 #     print (f"x is {x}")
 
 #
-while True:
-    try:
-        x = int(input("What's x? "))
-    except ValueError:
-        print("x is not integer")
-    else: #Executes if no error was found
-        break
+# while True:
+#     try:
+#         x = int(input("What's x? "))
+#     except ValueError:
+#         print("x is not integer")
+#     else: #Executes if no error was found
+#         break
 
-print (f"x is {x}")
+# print (f"x is {x}")
+
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            print("x is not integer")
+
+main()
