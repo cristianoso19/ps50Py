3fbfd8ed9c42f6718331d3f10357ed518534ac0e
cristianFedora <cristianoso19@gmail.com>
2023-11-03 22:40:32 -0500

    Else in try catch

--- exhello.py
+++ exhello.py
@@ -6,3 +6,13 @@ try:
     print (f"x is {x}")
 except ValueError:
     print("x is not integer")
+
+# NameError: problem with the names of varieables
+try:
+    x = int(input("What's x? "))
+except ValueError:
+    print("x is not integer")
+else: #Executes if no error was found
+    print (f"x is {x}")
+
+#
