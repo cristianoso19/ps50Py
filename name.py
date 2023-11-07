1cbb53425e7b5801f2f43539bbb2c70329ce6aaa
cristianFedora <cristianoso19@gmail.com>
2023-11-06 22:26:47 -0500

    Libraries: len(sys.argv )

--- name.py
+++ name.py
@@ -1,6 +1,8 @@
 import sys
 
-try:
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
     print("hello, my name is", sys.argv[1])
-except IndexError:
-    print("Too frew arguments")
