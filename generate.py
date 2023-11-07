bdd1dd5f8221a6f23b017b4547bc5828c1f821fa
cristianFedora <cristianoso19@gmail.com>
2023-11-06 22:02:16 -0500

    LIbraries: random.shuffle()

--- generate.py
+++ generate.py
@@ -1,5 +1,7 @@
 import random
 #from random import choice
 
-number = random.randint(1,10)
-print(number)
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
