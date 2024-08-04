f8be2336e0d0929a44956e98b37b576bb05f3217
cristianFedora <cristianoso19@gmail.com>
2024-08-04 14:18:04 -0500

    Checke errors

--- professor.py
+++ professor.py
@@ -1,13 +1,13 @@
 from random import randint
 
 def main():
-    level = get_level("Level: ")
     score = 0
+    level = get_level()
 
-    for i in range(9):
+    for i in range(10):
         num1 = generate_integer(level)
         num2 = generate_integer(level)
-        message = f"{num1} + {num2} = " 
+        message = f"{num1} + {num2} = "
         tries = 0
         response = 0
 
@@ -24,23 +24,23 @@ def main():
                         break
                     else:
                         raise ValueError
-                except ValueError:
+                except (ValueError, NameError):
                     print("EEE")
 
     print("Score:", score)
 
-def get_level(message):
+def get_level():
     while True:
         try:
-            int_number = int(input(message))
-            if (1 <= int_number <= 3):
-                break
+            n = int(input("Level: "))
+            if 1 <= n <= 3:
+                return n
         except:
             pass
-    return int_number 
+
 
 def generate_integer(level):
-    range_start = 10**(level-1)
+    range_start =  (10**(level-1))-1 if level == 1 else 10**(level-1)
     range_end = (10**level)-1
     return randint(range_start, range_end)
 
