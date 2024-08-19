1fd6f386dd3559325234654c7c055fde77ef8d81
cristianFedora <cristianoso19@gmail.com>
2024-08-18 21:57:25 -0500

    Sysexit bitcoin

--- bitcoin.py
+++ bitcoin.py
@@ -5,13 +5,13 @@ def main():
     i = 0
     if len(sys.argv) != 2:
         print("Missing command-line argument")
-        sys.exit() #terminate program
+        sys.exit(1) #terminate program
     else:
         try:
             i = float(sys.argv[1])
         except:
             print("Command-line argument is not a number")
-            sys.exit() #terminate program
+            sys.exit(1) #terminate program
 
     try:
         response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
